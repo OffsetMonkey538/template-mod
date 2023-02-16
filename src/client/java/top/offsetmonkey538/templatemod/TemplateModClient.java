40108ac05457ab7d7a0f3a0907e21bc9f2a75b91
OffsetMonkey538 <71213040+OffsetMonkey538@users.noreply.github.com>
2023-02-16 17:08:34 +0200

    Going to give up on trying to make meaningful commit messages because no one is even going to use this except me.

--- src/client/java/top/offsetmonkey538/templatemod/TemplateModClient.java
+++ src/client/java/top/offsetmonkey538/templatemod/TemplateModClient.java
@@ -2,7 +2,7 @@ package top.offsetmonkey538.templatemod;
 
 import net.fabricmc.api.ClientModInitializer;
 
-public class ExampleModClient implements ClientModInitializer {
+public class TemplateModClient implements ClientModInitializer {
 	@Override
 	public void onInitializeClient() {
 		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
