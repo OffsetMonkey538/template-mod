9f5d9a359316293694f62b1b5bd5043130c9a312
OffsetMonkey538 <71213040+OffsetMonkey538@users.noreply.github.com>
2023-02-24 18:58:19 +0200

    Add a 'MOD_ID' constant

--- src/main/java/top/offsetmonkey538/templatemod/TemplateMod.java
+++ src/main/java/top/offsetmonkey538/templatemod/TemplateMod.java
@@ -6,10 +6,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class TemplateMod implements ModInitializer {
-	// This logger is used to write text to the console and the log file.
-	// It is considered best practice to use your mod id as the logger's name.
-	// That way, it's clear which mod wrote info, warnings, and errors.
-	public static final Logger LOGGER = LoggerFactory.getLogger("template-mod");
+	public static final String MOD_ID = "templace-mod";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
 	public void onInitialize() {
